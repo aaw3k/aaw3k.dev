596a849697a68cf805f68c88a42669d2c40dfeb3
Sławek Jaskulski <Blejder4k@gmail.com>
2022-01-31 19:02:51 +0100

    Improve links.

--- components/Link/index.tsx
+++ components/Link/index.tsx
@@ -1,45 +1,44 @@
 import NextLink from 'next/link';
-import { cx } from 'lib/utils';
+import { cx, isExternalLink, isEmptyLink } from 'lib/utils';
 
 import styles from './Link.module.css';
 
 type Props = {
-  href: string;
+  to?: string;
+  href?: string;
   className?: string;
   underline?: boolean;
+  children: React.ReactNode;
 };
 
-export const Link: React.FC<Props> = ({
-  href,
-  className,
-  underline,
-  children,
-}) => {
-  const isInternal = (href: string) =>
-    href && (href.startsWith('/') || href.startsWith('#'));
+export const Link = ({ to, href, className, underline, children }: Props) => {
+  const url = to ?? (href as string);
 
-  const classes = cx(className, styles.root, {
+  const classes = cx(className, {
+    [styles.root]: href,
     [styles.underline]: underline,
   });
 
-  if (isInternal(href)) {
+  if (isExternalLink(url)) {
     return (
-      <NextLink href={href}>
-        <a className={classes}>{children}</a>
-      </NextLink>
-    );
-  }
-
-  return (
-    <>
       <a
         className={classes}
-        href={href}
+        href={url}
         rel="noopener noreferrer"
         target="_blank"
       >
         {children}
       </a>
-    </>
+    );
+  }
+
+  if (isEmptyLink(url)) {
+    return <div className={classes}>{children}</div>;
+  }
+
+  return (
+    <NextLink href={url}>
+      <a className={classes}>{children}</a>
+    </NextLink>
   );
 };
