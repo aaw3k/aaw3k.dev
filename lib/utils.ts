9cf3823e12ce0926da3d56c5756a944b1a845343
Sławek Jaskulski <Blejder4k@gmail.com>
2022-02-11 18:49:31 +0100

    Add prefers reduced motion media query.

--- lib/utils.ts
+++ lib/utils.ts
@@ -42,3 +42,16 @@ export const formatDate = ({
     formatted,
   };
 };
+
+export const prefersReducedMotion = (() => {
+  let shouldReduceMotion: boolean | undefined = undefined;
+
+  return () => {
+    if (shouldReduceMotion === undefined && typeof window !== 'undefined') {
+      const mediaQuery = matchMedia('(prefers-reduced-motion: reduce)');
+      shouldReduceMotion = !mediaQuery || mediaQuery.matches;
+    }
+
+    return shouldReduceMotion;
+  };
+})();
