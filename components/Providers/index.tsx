f1037a2d2e6911a71f5b9f66dc98a96e8c446c00
Sławek Jaskulski <Blejder4k@gmail.com>
2023-04-15 12:37:18 +0200

    Add subsets option to Inter font variable

--- components/Providers/index.tsx
+++ components/Providers/index.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from '@next/font/google';
 
-const inter = Inter({ variable: '--inter-font' });
+const inter = Inter({ subsets: ['latin'], variable: '--inter-font' });
 
 const StyleFont = () => (
   <style jsx global>{`
